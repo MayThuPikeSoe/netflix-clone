39d5b8c186151ffeac5ecc6428b32ebc9f87ca03
May Thu Pike Soe <maythupikesoe@gmail.com>
2024-08-12 22:57:00 +0630

    finished

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,26 +1,50 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   const base_url = "https://image.tmdb.org/t/p/original/";
+
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
       setMovies(request.data.results);
-      //   console.log(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
-  //   console.table(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          if (url) {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          }
+        })
+        .catch((error) => console.log("Trailer not found", error));
+    }
+  };
 
   return (
-    <div className="row ">
+    <div className="row">
       <h2 className="text-5xl font-bold py-14 text-white">{title}</h2>
-      <div className="row__posters object-contain flex overflow-scroll ">
+      <div className="row__posters object-contain flex overflow-scroll">
         {movies.map((movie) => (
           <img
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
@@ -29,10 +53,11 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
             }`}
             alt={movie.name}
             key={movie.id}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
-      <Youtube video={trailerUrl} opts={opts} />
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
